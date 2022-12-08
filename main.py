812c40ae36d202511f8696c8dd3c45f948440819
Your Name <yourgmail address>
2022-12-08 14:14:55 +0300

    num_checker

--- main.py
+++ main.py
@@ -4,12 +4,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = True
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+while word:
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+
+    else:
+        break
