ff917d6426d197dfabaafb5d081c49c8afa334ea
Your Name <yourgmail address>
2022-12-08 14:16:36 +0300

    num_checker

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = True
-
-while word:
+while True:
     try:
         word = input("Enter a word: ").upper()
         output_list = [phonetic_dict[letter] for letter in word]
